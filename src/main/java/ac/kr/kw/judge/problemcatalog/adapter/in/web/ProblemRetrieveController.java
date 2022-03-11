b7ac21d5faf4498b3248b45bce6fc8b6b08863a2
bugdebugging <annyousung@gmail.com>
2022-03-11 20:45:42 +0900

    feat: id목록 으로 조회 기능 추가

--- src/main/java/ac/kr/kw/judge/problemcatalog/adapter/in/web/ProblemRetrieveController.java
+++ src/main/java/ac/kr/kw/judge/problemcatalog/adapter/in/web/ProblemRetrieveController.java
@@ -2,13 +2,14 @@ package ac.kr.kw.judge.problemcatalog.adapter.in.web;
 
 import ac.kr.kw.judge.commons.api.ApiResult;
 import ac.kr.kw.judge.commons.api.ApiUtils;
-import ac.kr.kw.judge.problemcatalog.dto.in.ProblemSearchRequest;
 import ac.kr.kw.judge.problemcatalog.service.port.in.ProblemRetrieveService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,7 +22,7 @@ public class ProblemRetrieveController {
     }
 
     @GetMapping("/api/problem_catalogs/containing")
-    public ApiResult retrieveProblemContainingIds(@RequestBody ProblemSearchRequest problemSearchRequest) {
-        return ApiUtils.success(problemRetrieveService.findProblemContainingIds(problemSearchRequest.getProblemIds()));
+    public ApiResult retrieveProblemContainingIds(@RequestParam List<Long> problemIds) {
+        return ApiUtils.success(problemRetrieveService.findProblemContainingIds(problemIds));
     }
 }
