4b63e9aaf5a9d8ae813b4184fbc55075d4bd5031
bugdebugging <annyousung@gmail.com>
2022-03-12 20:22:40 +0900

    fix:bad request exception handle

--- src/main/java/ac/kr/kw/judge/commons/exception/GeneralControllerAdvice.java
+++ src/main/java/ac/kr/kw/judge/commons/exception/GeneralControllerAdvice.java
@@ -4,14 +4,25 @@ import ac.kr.kw.judge.commons.api.ApiResult;
 import ac.kr.kw.judge.commons.api.ApiUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 
 @RestControllerAdvice
 public class GeneralControllerAdvice {
     @ExceptionHandler({IllegalArgumentException.class,
             IllegalStateException.class})
     public ResponseEntity<ApiResult> handleBusinessException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ApiUtils.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class,
+            MissingServletRequestPartException.class,
+            HttpMediaTypeNotSupportedException.class})
+    public ResponseEntity<ApiResult> handleRequestException(Exception e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(ApiUtils.fail(HttpStatus.BAD_REQUEST.value(), e.getMessage()));
     }
